38333a7afd0b3a8a8ec3ed32042bf07a7cf8ca03
Crowdbotics <team@crowdbotics.com>
2023-01-07 08:32:07 +0000

    Initial commit.

---
+++ backend/versatile_express_f_38213/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for versatile_express_f_38213 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'versatile_express_f_38213.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "versatile-express-f-38213",
+  url: "https://versatile-express-f-38213.botics.co",
+  api: "https://versatile-express-f-38213.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
